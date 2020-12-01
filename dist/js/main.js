ceffcc6f550ea2c13d70952863bfbb5d18bdde29
boushib <boushib01@gmail.com>
2020-12-01 01:16:02 +0100

    remove jQuery dependency

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,15 @@
-$(function(){
-    // loading gif
-    $(window).on('load', function(){
-        $('#loading').fadeOut(1200);
-    });
-});
+window.onload = () => {
+  const spinner = document.getElementById('spinner')
+  const fadeEffect = setInterval(() => {
+    if (!spinner.style.opacity) {
+      spinner.style.opacity = 1
+    }
+    if (spinner.style.opacity > 0) {
+      spinner.style.opacity -= 0.05
+    } else {
+      clearInterval(fadeEffect)
+    }
+  }, 80)
+
+  fadeEffect()
+}
